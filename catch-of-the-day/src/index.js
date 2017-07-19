9e36816f3490c56c48647aae6f4bc2d4c68d6b9d
Jean Vasconcelos <jean.vasconcelos6@gmail.com>
2017-07-18 22:27:55 -0300

    Add App component to index

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,6 +1,7 @@
-import React from 'react'
-import { render } from 'react-dom'
-import "./css/style.css"
-import StorePicker from './components/StorePicker'
+import React from "react"
+import { render } from "react-dom"
 
-render(<StorePicker />, document.querySelector('#main'))
+import "./css/style.css"
+import App from "./components/App"
+
+render(<App />, document.querySelector("#main"))
