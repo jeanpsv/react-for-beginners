7f19f10face17e090e6e746050203d9f54647969
Jean Vasconcelos <jean.vasconcelos6@gmail.com>
2017-07-18 23:11:45 -0300

    Add default value to store picker button

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react"
+import { getFunName } from "../helpers"
 
 class StorePicker extends React.Component {
   render() {
     return (
       <form className="store-selector">
         <p>Please Enter A Store</p>
-        <input type="text" required placeholder="Store Name" />
+        <input type="text" required placeholder="Store Name"
+          defaultValue={getFunName()}/>
         <button type="submit">Visit Store</button>
       </form>
     )
