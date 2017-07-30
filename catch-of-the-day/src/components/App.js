694cd6fe766abe6243289cc0ef1c8342c8cc1755
Jean Vasconcelos <jean.vasconcelos6@gmail.com>
2017-07-30 17:21:58 -0300

    Allow update fishes data

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -12,6 +12,7 @@ class App extends React.Component {
     super()
 
     this.addFish = this.addFish.bind(this)
+    this.updateFish = this.updateFish.bind(this)
     this.loadSamples = this.loadSamples.bind(this)
     this.addToOrder = this.addToOrder.bind(this)
 
@@ -47,6 +48,12 @@ class App extends React.Component {
     const fishes = {...this.state.fishes}
     const timestamp = Date.now()
     fishes[`fish-${timestamp}`] = fish
+    this.setState({ fishes })
+  }
+
+  updateFish(key, updatedFish) {
+    const fishes = {...this.state.fishes}
+    fishes[key] = updatedFish
     this.setState({ fishes })
   }
 
@@ -87,6 +94,8 @@ class App extends React.Component {
         <Inventory
           addFish={this.addFish}
           loadSamples={this.loadSamples}
+          fishes={this.state.fishes}
+          updateFish={this.updateFish}
         />
       </div>
     )
